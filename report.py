3ada89526edffd085ef74e564d1b8d3573b58a0d
dusoft@gmail.com <dusoft@gmail.com>
2024-07-02 18:11:21 +0200

    added screenshot upload

--- report.py
+++ report.py
@@ -10,6 +10,9 @@ config=toml.load("config.toml")
 content={}
 command=subprocess.run(["/usr/bin/hostname"], universal_newlines = True, stdout = subprocess.PIPE)
 content["hostname"]=command.stdout.strip()
+if config["screenshot"] == True:
+    screenshot_filename=content["hostname"]+".png";
+    command=subprocess.run(["DISPLAY=:0 /usr/bin/scrot -o "+screenshot_filename], shell=True)
 command=subprocess.run(["/usr/bin/vcgencmd","measure_temp"], universal_newlines = True, stdout = subprocess.PIPE)
 content["temperature"]=command.stdout.strip()
 command=subprocess.run(["/usr/sbin/ifconfig",config["network"]], universal_newlines = True, stdout = subprocess.PIPE)
@@ -31,6 +34,10 @@ for item in command.stdout.split("\n"):
     if "chromium" in item:
         content["browser"]=item.strip()
         break
+
+if config["screenshot"] == True:
+    files = {'screenshot': open(screenshot_filename, 'rb')}
+    r = requests.post(config["receiver"], files=files)
 
 headers = {"Content-Type": "application/json"}
 json_data = json.dumps(content)
