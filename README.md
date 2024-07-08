0b11375ea44b63ff3b66b6a8aa62a4400304cada
dusoft@gmail.com <dusoft@gmail.com>
2024-07-08 13:58:03 +0200

    version 1, complete rewrite, fully configurable monitoring using bash commands

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # RPi Monitor Dashboard
 
-Raspberry Pi Monitor Dashboard is a simple monitoring tool with a dashboard suitable for monitoring multiple RPi devices. The number of devices you can monitor is unlimited.
+Raspberry Pi Monitor Dashboard is a simple monitoring tool with a dashboard suitable for monitoring multiple RPi devices. The number of devices you can monitor is unlimited. Fully configurable as to what report from bash / cli / terminal run.
 
-It currently reports these data:
+By default it reports these data:
 
 * hostname
 * CPU temperature
@@ -11,12 +11,14 @@ It currently reports these data:
 * running browser (only Firefox and Chromium are checked at the moment)
 * optionally, a screenshot of Pi's X screen (DISPLAY=:0)
 
+**It can report anything as commands are defined as standard bash commands and are fully configurable in config file.**
+
 ![image](https://github.com/nekromoff/rpi-monitor-dashboard/assets/8550349/dd7d2664-dc8a-43d8-ba7c-b2a08751fc94)
 
 ## Architecture
 
 * **Server** receiver and dashboard (one file) written in PHP (+config)
-* **Client** reporting script written in Python (+config in TOML)
+* **Client** reporting script (one file) written in Python (+config)
 
 ## Installation
 
@@ -25,16 +27,43 @@ It currently reports these data:
 2. Create `logs/` directory in the same path and make it writable (777)
 
 ### Client
-1. Rename `config.toml.example` to `config.toml` and edit: URL to server receiver path, network interface to check, optionally disable screenshot functionality
+1. Rename `config.toml.example` to `config.toml` and edit: URL to server receiver path and _optionally monitoring commands to execute_ (see Configuration in detail below)
 2. Upload `report.py` and `config.toml` to your Raspberry Pi (e.g. put in a new directory `/home/pi/rpi-monitor/`)
 3. Edit CRON (`crontab -e`) and add this line: `1 * * * * cd /home/pi/rpi-monitor/ && python3 report.py` (adjust reporting interval, if needed)
 
 **Open server dashboard URL in your browser and enjoy.**
 
+## Configuration explained
+
+### Server (config.php)
+1. You can change`$config['timezone']` to your timezone for dashboard to report correct time.
+2. You can set `$config['username']` and `$config['password']` to secure dashboard URL with login (digest in-browser authentication is used).
+
+Dashboard URL works without login by default.
+
+### Client (config.toml)
+Standard commands **with output** are put under `[commands]` section:
+
+This will run `uptime` command and fetch full output:
+
+```uptime = "/usr/bin/uptime"```
+
+This will run `ifconfig` command and fetch **only** output of line containing "inet ":
+
+```network = ["/usr/sbin/ifconfig wlan0", ["inet "] ]```
+
+This will run `ps` command and fetch **only** output of lines containing "firefox" or "chromium":
+
+```browser = ["/usr/bin/ps -A", ["firefox", "chromium"] ]```
+
+You can add as many strings to search for as you need. All those found will be joined into one string and reported back under the name of command (e.g. browser).
+
+Shell commands **without output** are put under `[commands_shell]` section (executed with `shell=True` in Python subprocess) - e.g. screenshot functionality.
+
 ## Dependencies
 * Python v3
 * Python modules:
     * subprocess, json, requests (should be available by default)
-    * toml (`pip3 install toml`)
-* If screenshots are enabled:
+    * tomli / tomllib (`pip3 install tomli` for Python <3.10, available by default from 3.11)
+* If screenshots are enabled, scrot is recommended:
     * scrot (`sudo apt install scrot`)
--- report.py
+++ report.py
@@ -1,45 +1,63 @@
 #!/usr/bin/env python3
+###########################################################
+## RPi Monitor Dashboard                                 ##
+## https://github.com/nekromoff/rpi-monitor-dashboard    ##
+## Copyright (c) 2024+ Daniel Duris, dusoft@staznosti.sk ##
+## License: MIT                                          ##
+## Version: 1.0                                          ##
+###########################################################
 
-import toml
+import tomli as tomllib
 import subprocess
 import json
 import requests
 
-config=toml.load("config.toml")
+with open("config.toml", "rb") as f:
+    config = tomllib.load(f)
 
 content={}
-command=subprocess.run(["/usr/bin/hostname"], universal_newlines = True, stdout = subprocess.PIPE)
-content["hostname"]=command.stdout.strip()
-if config["screenshot"] == True:
-    screenshot_filename=content["hostname"]+".png";
-    command=subprocess.run(["DISPLAY=:0 /usr/bin/scrot -o "+screenshot_filename], shell=True)
-command=subprocess.run(["/usr/bin/vcgencmd","measure_temp"], universal_newlines = True, stdout = subprocess.PIPE)
-content["temperature"]=command.stdout.strip()
-command=subprocess.run(["/usr/sbin/ifconfig",config["network"]], universal_newlines = True, stdout = subprocess.PIPE)
-for item in command.stdout.split("\n"):
-    if "inet" in item:
-        content["network"]=item.strip()
-        break
-command=subprocess.run(["/usr/bin/ping","-c4","8.8.8.8"], universal_newlines = True, stdout = subprocess.PIPE)
-for item in command.stdout.split("\n"):
-    if "packets" in item:
-        content["ping"]=item.strip()
-    if "rtt" in item:
-        content["ping"]=content["ping"]+"\n"+item.strip()
-command=subprocess.run(["/usr/bin/ps","-A"], universal_newlines = True, stdout=subprocess.PIPE)
-for item in command.stdout.split("\n"):
-    if "firefox" in item:
-        content["browser"]=item.strip()
-        break
-    if "chromium" in item:
-        content["browser"]=item.strip()
-        break
 
-if config["screenshot"] == True:
-    files = {'screenshot': open(screenshot_filename, 'rb')}
-    r = requests.post(config["receiver"], files=files)
+# always extract hostname
+output=subprocess.run("/usr/bin/hostname", universal_newlines = True, stdout = subprocess.PIPE)
+hostname=output.stdout.strip()
+content["hostname"]=hostname
+
+for name, command in config["commands"].items():
+    # simple command (TOML string)
+    if isinstance(command, str):
+        command=command.replace("{hostname}", hostname)
+        parts=command.split(" ")
+        output=subprocess.run(parts, universal_newlines = True, stdout = subprocess.PIPE)
+        content[name]=output.stdout.strip()
+    # command with text search and extraction (TOML array)
+    elif isinstance(command, list):
+        command[0]=command[0].replace("{hostname}", hostname)
+        parts=command[0].split(" ")
+        output=subprocess.run(parts, universal_newlines = True, stdout = subprocess.PIPE)
+        content[name]=""
+        for string in command[1]:
+            for line in output.stdout.split("\n"):
+                if string in line:
+                    content[name]=content[name]+line.strip()+"\n"
+    content[name]=content[name].strip()
+
+try:
+    for name, command in config["commands_shell"].items():
+        command=command.replace("{hostname}", hostname)
+        subprocess.run([command], shell=True)
+except Exception:
+    # ignore, skip
+    pass
+
+# if screenshot command exists, upload image
+try:
+    if config["commands_shell"]["screenshot"]:
+        files = {'screenshot': open(hostname+".png", 'rb')}
+        r = requests.post(config["receiver"], files=files)
+except Exception:
+    # ignore, skip
+    pass
 
 headers = {"Content-Type": "application/json"}
 json_data = json.dumps(content)
 response = requests.post(config["receiver"], data=json_data, headers=headers)
-#print(response.text)
