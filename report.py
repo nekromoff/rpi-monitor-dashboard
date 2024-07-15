98bcac4ce6bdf935bec847d6eba9b2664abeee68
dusoft@gmail.com <dusoft@gmail.com>
2024-07-15 19:37:38 +0200

    added custom user agent+version and exception to ignore unknown bash commands

--- report.py
+++ report.py
@@ -12,6 +12,8 @@ import subprocess
 import json
 import requests
 
+_version="1.0"
+
 with open("config.toml", "rb") as f:
     config = tomllib.load(f)
 
@@ -23,23 +25,27 @@ hostname=output.stdout.strip()
 content["hostname"]=hostname
 
 for name, command in config["commands"].items():
-    # simple command (TOML string)
-    if isinstance(command, str):
-        command=command.replace("{hostname}", hostname)
-        parts=command.split(" ")
-        output=subprocess.run(parts, universal_newlines = True, stdout = subprocess.PIPE)
-        content[name]=output.stdout.strip()
-    # command with text search and extraction (TOML array)
-    elif isinstance(command, list):
-        command[0]=command[0].replace("{hostname}", hostname)
-        parts=command[0].split(" ")
-        output=subprocess.run(parts, universal_newlines = True, stdout = subprocess.PIPE)
-        content[name]=""
-        for string in command[1]:
-            for line in output.stdout.split("\n"):
-                if string in line:
-                    content[name]=content[name]+line.strip()+"\n"
-    content[name]=content[name].strip()
+    try:
+        # simple command (TOML string)
+        if isinstance(command, str):
+            command=command.replace("{hostname}", hostname)
+            parts=command.split(" ")
+            output=subprocess.run(parts, universal_newlines = True, stdout = subprocess.PIPE)
+            content[name]=output.stdout.strip()
+        # command with text search and extraction (TOML array)
+        elif isinstance(command, list):
+            command[0]=command[0].replace("{hostname}", hostname)
+            parts=command[0].split(" ")
+            output=subprocess.run(parts, universal_newlines = True, stdout = subprocess.PIPE)
+            content[name]=""
+            for string in command[1]:
+                for line in output.stdout.split("\n"):
+                    if string in line:
+                        content[name]=content[name]+line.strip()+"\n"
+        content[name]=content[name].strip()
+    except Exception:
+        # ignore, skip
+        pass
 
 try:
     for name, command in config["commands_shell"].items():
@@ -49,15 +55,17 @@ except Exception:
     # ignore, skip
     pass
 
+headers = {"User-Agent": "RPi Monitor Dashboard/"+_version}
+
 # if screenshot command exists, upload image
 try:
     if config["commands_shell"]["screenshot"]:
         files = {'screenshot': open(hostname+".png", 'rb')}
-        r = requests.post(config["receiver"], files=files)
+        r = requests.post(config["receiver"], files=files, headers=headers)
 except Exception:
     # ignore, skip
     pass
 
-headers = {"Content-Type": "application/json"}
+headers["Content-Type"]="application/json"
 json_data = json.dumps(content)
 response = requests.post(config["receiver"], data=json_data, headers=headers)
